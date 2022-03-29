90221bd646c0388347ab31211e66a771a4691c20
Мария <1481047@mail.ru>
2022-03-29 12:51:39 +0300

    h2

--- CreationModelPlugin/CreationModel.cs
+++ CreationModelPlugin/CreationModel.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
@@ -13,16 +14,16 @@ namespace CreationModelPlugin
     public class CreationModel : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
-        {         
-
-            CreatWall(commandData);                    
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            CreatWall(doc);
 
 
             return Result.Succeeded;
         }
-        public Level GetLevel1(ExternalCommandData commandData)
+        public Level GetLevel1(Document doc)
         {
-            Document doc = commandData.Application.ActiveUIDocument.Document;
+
             List<Level> listLevel = new FilteredElementCollector(doc)
                   .OfClass(typeof(Level))
                   .OfType<Level>()
@@ -30,26 +31,23 @@ namespace CreationModelPlugin
             var level1 = listLevel
                 .Where(l => l.Name.Equals("Уровень 1"))
                 .FirstOrDefault();
-            
+
             return level1;
         }
-        public Level GetLevel2(ExternalCommandData commandData)
+        public Level GetLevel2(Document doc)
         {
-            Document doc = commandData.Application.ActiveUIDocument.Document;
             List<Level> listLevel = new FilteredElementCollector(doc)
                   .OfClass(typeof(Level))
                   .OfType<Level>()
                   .ToList();
-            
+
             var level2 = listLevel
            .Where(l => l.Name.Equals("Уровень 2"))
            .FirstOrDefault();
             return level2;
         }
-        public void CreatWall(ExternalCommandData commandData)
+        public void CreatWall(Document doc)
         {
-            Document doc = commandData.Application.ActiveUIDocument.Document;
-
             double width = UnitUtils.ConvertToInternalUnits(10000, UnitTypeId.Millimeters);
             double depth = UnitUtils.ConvertToInternalUnits(5000, UnitTypeId.Millimeters);
             double dx = width / 2;
@@ -69,15 +67,66 @@ namespace CreationModelPlugin
                 for (int i = 0; i < 4; i++)
                 {
                     Line line = Line.CreateBound(points[i], points[i + 1]);
-                    Wall wall = Wall.Create(doc, line, GetLevel1(commandData).Id, false);
+                    Wall wall = Wall.Create(doc, line, GetLevel1(doc).Id, false);
                     walls.Add(wall);
-                    wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(GetLevel2(commandData).Id);
+                    wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(GetLevel2(doc).Id);
                 }
-             
-                ts.Commit();                
+                AddDoors(doc, walls[0]);
+                AddWindows(doc, walls);
+                ts.Commit();
             }
-            
+
 
         }
+
+        private void AddWindows(Document doc, List<Wall> walls)
+        {
+            var windowsType = new FilteredElementCollector(doc)
+                   .OfClass(typeof(FamilySymbol))
+                   .OfCategory(BuiltInCategory.OST_Windows)
+                   .OfType<FamilySymbol>()
+                   .Where(w => w.Name.Equals("0406 x 1220 мм"))
+                   .Where(w => w.FamilyName.Equals("M_Неподвижный"))
+                   .FirstOrDefault();
+
+            for (int i = 1; i < walls.Count; i++)
+            {               
+                var wall = walls[i];
+                
+                LocationCurve hostCurve = wall.Location as LocationCurve;
+
+                XYZ point1 = hostCurve.Curve.GetEndPoint(0);
+                XYZ point2 = hostCurve.Curve.GetEndPoint(1);
+                XYZ point = (point1 + point2) / 2;
+                XYZ point3 = new XYZ(point.X, point.Y, 3);
+
+                if (!windowsType.IsActive)
+                    windowsType.Activate();
+                doc.Create.NewFamilyInstance(point3, windowsType, wall, GetLevel1(doc), StructuralType.NonStructural);
+                
+            }
+
+        }
+
+        private void AddDoors(Document doc, Wall wall)
+        {
+            var doorType = new FilteredElementCollector(doc)
+                   .OfClass(typeof(FamilySymbol))
+                   .OfCategory(BuiltInCategory.OST_Doors)
+                   .OfType<FamilySymbol>()
+                   .Where(d => d.Name.Equals("0915 x 2032 мм"))
+                   .Where(d => d.FamilyName.Equals("M_Однопольные-Щитовые"))
+                   .FirstOrDefault();
+
+            LocationCurve hostCurve = wall.Location as LocationCurve;
+
+            XYZ point1 = hostCurve.Curve.GetEndPoint(0);
+            XYZ point2 = hostCurve.Curve.GetEndPoint(1);
+            XYZ point = (point1 + point2) / 2;
+            if (!doorType.IsActive)
+                doorType.Activate();
+            doc.Create.NewFamilyInstance(point, doorType, wall, GetLevel1(doc), StructuralType.NonStructural);
+        }
+
     }
 }
